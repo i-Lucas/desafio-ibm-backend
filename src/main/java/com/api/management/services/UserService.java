7bc78a007779d35641a051daf20ec8d31d2e3891
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-12 19:29:58 -0300

    refactor: get user by token method

--- src/main/java/com/api/management/services/UserService.java
+++ src/main/java/com/api/management/services/UserService.java
@@ -1,10 +1,9 @@
 package com.api.management.services;
 
 import com.api.management.dtos.UserDto;
+import com.api.management.exceptions.user.UserNotFoundException;
 import com.api.management.models.UserModel;
 import com.api.management.repositories.UserRepository;
-
-import io.jsonwebtoken.JwtException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,7 +25,7 @@ public class UserService {
     public ResponseEntity<Object> signup(UserDto userDto) {
 
         if (this.checkEmailAlreadyRegistered(userDto.getEmail())) {
-            return this.getResponseEntityObject(HttpStatus.CONFLICT, "E-mail already exists.");
+            return this.getResponseEntityObject(HttpStatus.CONFLICT, "E-mail already exists.", "error");
         }
 
         var hashPassword = authService.hashPassword(userDto.getPassword());
@@ -35,63 +34,57 @@ public class UserService {
         return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully.");
     }
 
-    private boolean validateUserPassword(UserDto userDto, UserModel user) {
-        return authService.validatePassword(userDto.getPassword(), user.getPassword());
-    }
-
     public ResponseEntity<Object> signin(UserDto userDto) {
 
         UserModel user = userRepository.findByEmail(userDto.getEmail());
 
         if (user == null || !this.validateUserPassword(userDto, user)) {
-            return this.getResponseEntityObject(HttpStatus.UNAUTHORIZED, "Invalid email or password.");
+            return this.getResponseEntityObject(HttpStatus.UNAUTHORIZED, "Invalid email or password.", "error");
         }
 
-        var token = this.authService.generateToken(user.getEmail());
+        int minutes = 60;
+        var token = this.authService.generateToken(user.getEmail(), minutes);
         return this.getResponseEntityObject(HttpStatus.OK, token, "token");
     }
 
-    public ResponseEntity<Object> getUserEmailByToken(String token) {
+    public ResponseEntity<Object> getUserDetails(String token) {
 
-        String jwtToken = token.substring(7); // Remove "Bearer "
+        var user = this.getUserByToken(token);
+        var response = new HashMap<>();
 
-        try {
-
-            String email = authService.extractEmailFromToken(jwtToken);
-            UserModel user = userRepository.findByEmail(email);
-
-            if (user == null) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found.");
-            }
-
-            var response = this.getHashmap();
-            response.put("email", user.getEmail());
-            return ResponseEntity.ok(response);
-
-        } catch (JwtException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token.");
-        }
-    }
-
-    private ResponseEntity<Object> getResponseEntityObject(HttpStatus status, String message) {
-        var response = this.getHashmap();
-        response.put("status", status.toString());
-        response.put("message", message);
-        return ResponseEntity.status(status).body(response);
+        response.put("email", user.getEmail());
+        response.put("id", user.getId().toString());
+        return ResponseEntity.ok(response);
     }
 
     private ResponseEntity<Object> getResponseEntityObject(HttpStatus status, String message, String type) {
-        var response = this.getHashmap();
-        response.put("status", status.toString());
-        response.put(type, message);
-        return ResponseEntity.status(status).body(response);
-    }
-
-    private Map<String, String> getHashmap() {
-        return new HashMap<>();
+        return ResponseEntity.status(status)
+                .body(Map.of("status", status.toString(), type, message));
     }
 
     private boolean checkEmailAlreadyRegistered(String email) {
         return userRepository.findByEmail(email) != null;
+    }
+
+    private boolean validateUserPassword(UserDto userDto, UserModel user) {
+        return authService.validatePassword(userDto.getPassword(), user.getPassword());
+    }
+
+    private UserModel getUserByToken(String token) {
+
+        String jwtToken = token.substring(7);
+        String email = authService.extractEmailFromToken(jwtToken);
+
+        if (email == null) {
+            throw new UserNotFoundException();
+        }
+
+        UserModel user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UserNotFoundException();
+        }
+
+        return user;
     }
 }
