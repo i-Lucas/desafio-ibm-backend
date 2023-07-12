708a6cee998d9304dcb569bdfad4f357de7b21c4
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-12 02:07:23 -0300

    feat: authcontroller

---
+++ src/main/java/com/api/management/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package com.api.management.controllers;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.api.management.dtos.UserDto;
+import com.api.management.services.UserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody @Valid UserDto userDto) {
+        return this.userService.signup(userDto);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody @Valid UserDto userDto) {
+        return this.userService.signin(userDto);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<?> getUserDetails(@RequestHeader("Authorization") String token) {
+        return userService.getUserEmailByToken(token);
+    }
+
+}
