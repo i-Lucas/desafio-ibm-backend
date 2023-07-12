f3d6532be379eed901a6efc561138ae6384d8e79
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-12 02:26:29 -0300

    refactor: remove unnecessary get and set methods

--- src/main/java/com/api/management/dtos/UserDto.java
+++ src/main/java/com/api/management/dtos/UserDto.java
@@ -1,25 +1,11 @@
 package com.api.management.dtos;
 
+import lombok.Data;
+
+@Data
 public class UserDto {
 
     private String email;
     private String password;
 
-    // Getters and Setters
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
