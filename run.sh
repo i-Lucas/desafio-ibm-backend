7e9373b4aa827e7ce5cba8ab0ec869141179be95
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-13 07:58:06 -0300

    fix: add global config for cors

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! command -v mvn &> /dev/null; then
+  echo "Erro: Maven não encontrado. Certifique-se de ter o Maven instalado no seu sistema."
+  exit 1
+fi
+
+if [ "$1" = "-test" ]; then
+  mvn test
+else
+   cd target/ && java -jar management-0.0.1-SNAPSHOT.jar
+fi
--- src/main/java/com/api/management/CandidateManagementApplication.java
+++ src/main/java/com/api/management/CandidateManagementApplication.java
@@ -2,6 +2,9 @@ package com.api.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CandidateManagementApplication {
@@ -10,4 +13,18 @@ public class CandidateManagementApplication {
 		SpringApplication.run(CandidateManagementApplication.class, args);
 	}
 
+}
+
+@Configuration
+class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization")
+                .exposedHeaders("Content-Type", "Authorization")
+                .maxAge(3600);
+    }
 }
