63c90197b838e0b7e94b56bfbc101251ca23d1d4
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-13 05:44:54 -0300

    feat: user repository custom query

--- src/main/java/com/api/management/repositories/UserRepository.java
+++ src/main/java/com/api/management/repositories/UserRepository.java
@@ -1,11 +1,17 @@
 package com.api.management.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.api.management.models.CandidateModel;
 import com.api.management.models.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
     UserModel findByEmail(String email);
+
+    @Query("SELECT c FROM CandidateModel c WHERE c.userModel = ?1")
+    List<CandidateModel> findAllCandidatesByUser(UserModel user);
 }
