b27fe321aa7470986a100071af591ac7f8d14963
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-13 05:44:25 -0300

    feat: user model candidate relationship

--- src/main/java/com/api/management/models/UserModel.java
+++ src/main/java/com/api/management/models/UserModel.java
@@ -1,11 +1,14 @@
 package com.api.management.models;
 
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,4 +30,7 @@ public class UserModel {
 
     private String email;
     private String password;
+
+    @OneToMany(mappedBy = "userModel", cascade = CascadeType.ALL)
+    private List<CandidateModel> candidates;
 }
